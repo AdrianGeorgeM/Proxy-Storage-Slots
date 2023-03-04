5eb1ca9cffc09bb2b7f063078be8866d7be78a35
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-03-04 20:05:03 +0000

    Update/Deploy  Contract  to my local blockchain

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -2,9 +2,7 @@
 pragma solidity ^0.8.9;
 
 contract Storage {
-   
-    }
-
-
-
- 
+    uint256 x = 97; // 0x0 -> 0x61
+    uint256 y = 98; // 0x1 -> 0x62
+    // mapping(uint => uint) name; // 0x2 -> 0x2
+}
