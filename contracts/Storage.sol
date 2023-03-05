417818569238a26020829d0c051de9440027c83f
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-03-05 14:18:51 +0000

    Created key and baseSlot for the contructor

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.18;
 
 contract Storage {
     uint256 x = 97; // 0x0 -> first storage slot
     uint256 y = 98; // 0x1 -> 0x62
     // mapping(uint => uint) name; // 0x2 -> 0x2
+    uint256 z = 99; // 0x2 -> 0x2
+
+    mapping(uint => uint) public testing; // 0x3 -> 0x3
+
+    constructor() {
+        testing[21] = 77;
+        testing[44] = 98;
+    }
 }
--- scripts/lookup.js
+++ scripts/lookup.js
@@ -1,10 +1,16 @@
 const hre = require('hardhat');
-const address = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-
+const { keccak256, hexZeroPad } = hre.ethers.utils;
+const address = '  0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9';
+//deployed contract address from deploy.js
 // This is a simple script to get the storage of a contract
 async function lookup() {
-	//eth_getStorageAt
-	const storage = await hre.ethers.provider.getStorageAt(address, '0x0');
+	//eth_getStorageA t
+	const key = hexZeroPad(21, 32);
+	const baseSlot = hexZeroPad(0x2, 32).slice(2);
+
+	// console.log(`Key: ${parseInt(key)} Base Slot: ${baseSlot}`);
+	const slot = keccak256(key + baseSlot);
+	const storage = await hre.ethers.provider.getStorageAt(address, slot);
 	console.log(`Storage at 0x0: ${parseInt(storage)}`); //0x61 is hex for 97
 	// run node to see the storage at 0x61
 }
